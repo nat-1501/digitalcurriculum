a070dc2f2e242f08191c11156711d3799cecb4a4
nat-1501 <natalisoares90@gmail.com>
2022-02-08 17:21:59 -0300

    criando coluna

--- digitalcurriculum/src/App.js
+++ digitalcurriculum/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './style.css';
+
+function app(){
+  return(
+    <div className="coluna">
+        <h1 className="title">Contato</h1>
+        <span>ENDEREÇO</span>
+        <span>TELEFONE</span>
+        <span>EMAIL</span>
+        <span>NACIONALIDADE</span>
+
+
+    
+
+        <div className="coluna">
+      
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default app 
