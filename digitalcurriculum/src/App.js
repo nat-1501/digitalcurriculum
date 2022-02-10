cff33249432866f01314abe8d39e6baa91460803
nat-1501 <natalisoares90@gmail.com>
2022-02-10 11:11:12 -0300

    finalizada a parte de CONTATO

--- digitalcurriculum/src/App.js
+++ digitalcurriculum/src/App.js
@@ -1,15 +1,31 @@
 
 import './style.css';
+import Imagem from './Imagem.png'
 
 function app(){
   return(
     <div className="cv">
-        <div className="coluna">
-          <h1>C O N T A T O</h1>
-        <span>ENDEREÇO</span>
-        <span>TELEFONE</span>
-        <span>EMAIL</span>
-        <span>NACIONALIDADE</span>
+      
+      <div className="coluna">
+          <img className="img1" alt='img' src={Imagem.png}/>
+            <h1>C O N T A T O</h1>
+            
+            <div className="endereco">
+              <span>ENDEREÇO</span>
+              <p> Travessa Nilo Chagas 09, Fazenda da juta, São Paulo - SP </p> 
+            </div>
+            <div className="tel">
+              <span>CELULAR</span>
+              <p>(11) 98427-1548</p>
+            </div>
+            <div className="email">
+              <span>EMAIL</span>
+              <p> natalisoares90@gmail.com</p>
+            </div>
+            <div className="nac">
+             <span>NACIONALIDADE</span>
+             <p>Brasileira </p>
+            </div> 
       </div>
     </div>
   )
