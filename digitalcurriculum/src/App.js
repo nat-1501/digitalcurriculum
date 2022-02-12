3317eb64cf5efa01b1b7a33a74351d6769349634
nat-1501 <natalisoares90@gmail.com>
2022-02-12 19:44:56 -0300

    finalizada a parte de contato e perfil

--- digitalcurriculum/src/App.js
+++ digitalcurriculum/src/App.js
@@ -1,42 +1,54 @@
 
 import './style.css';
-import Imagem from './Imagem.png'
+import Imagem from './image-photo.png'
 
 function app(){
   return(
     <div className="cv">
       
       <div className="coluna">
-          <img className="img1" alt='img' src={Imagem.png}/>
-            <h1>C O N T A T O</h1>
+          <img className="img1" alt='img' src={Imagem}/>
+            <h1>CONTATO</h1>
             
-            <div className="endereco">
+            <div className="informacoes">
               <span>ENDEREÇO</span>
               <p> Travessa Nilo Chagas 09, Fazenda da juta, São Paulo - SP </p> 
             </div>
-            <div className="tel">
+
+            <div className="informacoes">
               <span>CELULAR</span>
               <p>(11) 98427-1548</p>
             </div>
-            <div className="email">
+
+            <div className="informacoes">
               <span>EMAIL</span>
               <p> natalisoares90@gmail.com</p>
             </div>
-            <div className="nac">
-             <span>NACIONALIDADE</span>
-             <p>Brasileira </p>
+
+            <div className="informacoes">
+              <span>NACIONALIDADE</span>
+              <p>Brasileira </p>
             </div> 
-            <div className="colunadir">
+
+            <div className="separador">
+              
+            </div>        
+
+            <div className="perfil">
+              <h1>PERFIL</h1>
+              <p>Focada sempre em ser e mostrar meu melhor, tenho habilidades como boa comunicação, determinação. Apaixonada por estudos e leitura, não tenho experiencia na área, porém tenho muito sangue nos olhos para mostrar tudo o que eu tenho aprendido e sede de absorver o que puder para me tornar uma profisional de requinte no mercado de trabalho. </p>    
+
+            </div>
+      </div> 
+    
+      <div className="colunadir">
               <h1>NATALI SOARES</h1>
               <p> Desenvolvedora Full Stack / front end</p>
             <div className= "expec">
               <h2>EDUCAÇÃO</h2>
-              <p>2018 - 2020 / Produção Multimidia (incompleto</p>
+              <p>2018 - 2020 / Produção Multimidia (incompleto)</p>
             </div>    
             </div>
-            
-      </div>
-      
     </div>
   )
 }
